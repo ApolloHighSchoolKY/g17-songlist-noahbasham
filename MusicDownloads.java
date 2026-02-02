e213f9e27075a3c65fdc6f07e288bbaa9f8247fc
noahbasham <noah.basham@stu.daviess.kyschools.us>
2026-02-02 18:10:32 +0000

    commit

--- MusicDownloads.java
+++ MusicDownloads.java
@@ -7,15 +7,27 @@ public class MusicDownloads{
   public DownloadInfo getDownloadInfo(String title)
   {
     /* To be implemented in part (a) */
+    for(DownloadInfo x: downloadList)
+    {
+      if(title.equals(x.getTitle()))
+        return x;
+    }
 
+    return null;
 
   }
 
   public void updateDownloadInfo(List<String> titles)
   {
     /* To be implemented in part (b) */
-
-
-
+    for(int i=0;i<titles.length();i++)
+    {
+      if(titles.get(i).getDownloadInfo==null)
+      {
+        downloadList.add(new DownloadInfo(titles.get(i)));
+      }
+      else
+        titles.get(i).getDownloadInfo.incrementTimesDownloaded();
+    }
   }
 }
